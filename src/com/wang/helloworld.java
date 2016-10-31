8635412b1058f88fbd09874f1b7bf111f7728175
wangjie <915179659@qq.com>
2016-10-31 18:15:59 +0800

    Initial commit

---
+++ src/com/wang/helloworld.java
@@ -0,0 +1,10 @@
+package com.wang;
+
+/**
+ * Created by WJPC on 2016/10/31.
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
